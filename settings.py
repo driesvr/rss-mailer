fd399f13451efb0cf6009e9c890b3cde75b6ac90
driesvr <dries.vanrompaey@gmail.com>
2015-12-17 15:35:23 +0100

    Add example

---
+++ settings.py
@@ -0,0 +1,8 @@
+sender = 'sender@emailadress.com'
+receiver = 'receiver@emailadress.com'
+password = 'sender_password'
+hostname = 'smtp.emailadress.com'
+max_article_age = 24
+
+list_of_my_journals = ['http://feeds.feedburner.com/acs/jmcmar',
+'http://feeds.nature.com/nrd/rss/aop']
